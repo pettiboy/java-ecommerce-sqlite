7e4210aedb73d3e365098e3deea8097d5d102c3d
Hussain Pettiwala <hussain.pettiwala@gmail.com>
2021-11-30 17:55:23 +0530

    seperate classes into different files

--- src/Order.java
+++ src/Order.java
@@ -1,7 +1,5 @@
 package src;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.Date;
 import java.util.Scanner;
 import java.util.Vector;
@@ -80,62 +78,6 @@ public class Order {
 
     public String csvString(Order order) {
         return "(" + order.user.id + ", '" + order.dateOrdered + "', '" + order.complete + "');";
-    }
-
-}
-
-class Orders {
-    Vector<Order> orders = new Vector<>();
-
-    Orders() {
-        Products products = new Products();
-        try {
-            // get all completed orders
-            ResultSet allOrders = Connect.runQuery("SELECT * FROM orders WHERE complete = 'true';");
-            // loop over allOrders
-            while (allOrders.next()) {
-                // get user and product
-                User user = new User(allOrders.getString("userId"), new Scanner(System.in));
-                Order order = new Order(user);
-
-                order.id = Integer.parseInt(allOrders.getString("id"));
-
-                // get all product ids for this product
-                ResultSet allProductIds = Connect.runQuery("SELECT productId FROM order_product WHERE orderId = "+ order.id + ";");
-                // loop over each productId
-                while (allProductIds.next()) {
-                    // convert productId to Product object
-                    Product selectedProduct = products.productIdToProduct(Integer.parseInt(allProductIds.getString("productId")));
-                    // add that product to cart
-                    order.addToCart(selectedProduct);
-                }
-
-                // save order's date and complete states
-                order.dateOrdered = allOrders.getString("dateOrdered");
-                order.complete = Boolean.parseBoolean(allOrders.getString("complete"));
-
-                // add order to orders vector
-                this.orders.add(order);
-            }
-        } catch (SQLException e1) {
-            e1.printStackTrace();
-        }
-    }
-
-    public Vector<Order> getAllOrders() {
-        return this.orders;
-    }
-
-    public void printCartOf(int orderId) {
-        Order useOrder;
-        for (Order order : this.orders) {
-            if (order.id == orderId) {
-                useOrder = order;
-                useOrder.getCartItems();
-                return;
-            }
-        }
-        Print.print("Invalid Order ID...", Print.RED);
     }
 
 }
--- src/Products.java
+++ src/Products.java
@@ -73,27 +73,4 @@ public class Products {
 
 }
 
-class Product {
-    Integer id;
-    String name;
-    Double price;
-    String description;
-    boolean isActive = true;
 
-    Product(Integer id, String name, double price, String description) {
-        this.id = id;
-        this.name = name;
-        this.price = price;
-        this.description = description;
-    }
-
-    public String toString() {
-        return "ID: " + this.id.toString() + "\n" + "Name: " + this.name + "\n" + "Price: " + this.price.toString()
-                + "\n" + "Description: " + this.description.toString();
-    }
-
-    public String csvString() {
-        return "('" + this.name + "', '" + this.price.toString() + "', '" + this.description + "', '" + this.isActive
-                + "')";
-    }
-}
